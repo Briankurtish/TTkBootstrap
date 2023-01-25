98c71bbff4ee5a40b56f3f05f1357238af8018b0
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-25 19:40:30 +0300

    created a simple counting command for the button

--- tb_intro.py
+++ tb_intro.py
@@ -2,6 +2,17 @@ from tkinter import *
 from ttkbootstrap.constants import *
 import ttkbootstrap as tb
 
+counter = 0
+
+
+def changer():
+    global counter
+    counter += 1
+    if counter % 2 == 0:
+        my_label.config(text="Hello World")
+    else:
+        my_label.config(text="Goodbye World")
+
 
 root = tb.Window(themename="superhero")
 root.title("TTk Bootstrap")
@@ -14,7 +25,7 @@ my_label = tb.Label(text="Hello World", font=(
 my_label.pack(pady=50)
 
 my_button = tb.Button(
-    text="Click Me!", bootstyle="success, outline", command=())
+    text="Click Me!", bootstyle="success, outline", command=changer)
 my_button.pack(pady=20)
 
 root.mainloop()
