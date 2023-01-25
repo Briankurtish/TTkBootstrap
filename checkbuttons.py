f86e468510a852f0af03f09327101a32cd17609e
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-25 20:26:26 +0300

    created round toggle button

--- checkbuttons.py
+++ checkbuttons.py
@@ -42,4 +42,12 @@ my_check3 = tb.Checkbutton(text="ToolButton!", bootstyle=(
     "success, toolbutton, outline"), variable=var3, offvalue=0, onvalue=1, command=checker)
 my_check3.pack(pady=10)
 
+# Round Toggle Button
+
+var4 = IntVar()
+
+my_check4 = tb.Checkbutton(text="Round Toggle Button", bootstyle=(
+    "info, round-toggle"), variable=var4, offvalue=0, onvalue=1, command=checker)
+my_check4.pack(pady=10)
+
 root.mainloop()
