f723a3ed35e7df4166dd9a857ea106c4e6ab65aa
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-25 21:10:03 +0300

    code on how to increase button size

---
+++ button_size.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import ttkbootstrap as tb
+
+
+root = tb.Window(themename="superhero")
+root.title("Button Size")
+root.geometry("500x350")
+
+# Style
+my_style = tb.Style()
+my_style.configure('success.TButton', font=("Helvetica", 18))
+
+my_button = tb.Button(text="Hello World",
+                      bootstyle="success", style='success.TButton')
+my_button.pack(pady=20)
+
+root.mainloop()
