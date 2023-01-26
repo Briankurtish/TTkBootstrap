b3ed60ac576a9bc7f11d14601bfecf5b5d547e9b
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 20:38:56 +0300

    created titile label

--- ttkbootsrapp Login/login.py
+++ ttkbootsrapp Login/login.py
@@ -7,4 +7,10 @@ root.title("Login")
 root.geometry('900x700')
 
 
+# Create Top Label
+
+title = tb.Label(root, text="Welcome Cipher!", font=(
+    "Heveltica", 25), bootstyle="light")
+title.pack(pady=20)
+
 root.mainloop()
