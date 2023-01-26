f3e4c95534df58850dff15f63652a77ab2087892
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 19:57:48 +0300

    created speak function linked to the button

--- entry_widget.py
+++ entry_widget.py
@@ -8,13 +8,17 @@ root.geometry("500x350")
 # Create Entry function
 
 
+def speak():
+    my_label.config(text=f"You typed: {my_entry.get()}")
+
+
 # Create Entry Widget
 my_entry = tb.Entry(root)
 my_entry.pack(pady=20)
 
 # Create Button
 my_button = tb.Button(root, text="Click Me!",
-                      bootstyle="danger outline", command=())
+                      bootstyle="danger outline", command=speak)
 my_button.pack(pady=20)
 
 # Create Label
