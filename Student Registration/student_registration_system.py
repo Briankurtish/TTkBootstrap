fcd76e15a1170a87d3faf2197106b009f9cf0db4
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 23:10:26 +0300

    created top heading label

--- Student Registration/student_registration_system.py
+++ Student Registration/student_registration_system.py
@@ -7,10 +7,15 @@ root.title("Student registration System")
 root.geometry("500x600")
 
 head_frame = tb.Frame(root)
+
+heading_lb = tb.Label(head_frame, text="Student Registration System", font=(
+    "Bold", 13), bootstyle="danger")
+heading_lb.pack(pady=5)
+
 head_frame.pack(pady=10)
 # When you use pack_propagate you can resize the Frame like a container and give it a width and height
 head_frame.pack_propagate(False)
-head_frame.config(width=500, height=600)
+head_frame.config(width=400, height=300)
 
 
 root.mainloop()
