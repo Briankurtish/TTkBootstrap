9f7acf6273de7db234ef45cf7f3ea2f37a1ce5ef
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 19:52:24 +0300

    created entry.py file and structure

---
+++ entry_widget.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import ttkbootstrap as tb
+
+root = tb.Window(themename="superhero")
+root.title("Entry Widget")
+root.geometry("500x350")
+
+# Create Entry function
+
+
+# Create Entry Widget
+
+# Create Button
+
+
+# Create Label
+
+
+root.mainloop()
