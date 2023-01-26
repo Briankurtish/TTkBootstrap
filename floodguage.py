84f4ac27b9c4ccf67d0f8afb6ee43509a496fbf6
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 20:12:38 +0300

    created Floodguaue

--- floodguage.py
+++ floodguage.py
@@ -5,5 +5,11 @@ root = tb.Window(themename="superhero")
 root.title("Floodguage")
 root.geometry("500x350")
 
+# Create Floodgauge
+
+my_gauue = tb.Floodgauge(root, bootstyle="success",
+                         font=("Helvetica", 18), mask="Pos: {}%", maximum=100, orient="horizontal", value=10)
+my_gauue.pack(pady=20)
+
 
 root.mainloop()
