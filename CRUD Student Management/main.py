249c25e31fdbc4153146d121209f382c6a3c5496
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-27 02:17:16 +0300

    first code

---
+++ CRUD Student Management/main.py
@@ -0,0 +1,17 @@
+import pymysql
+from tkinter import *
+import ttkbootstrap as tb
+from tkinter import ttk
+from tkinter import messagebox
+
+
+# connection to database
+
+
+# GUI
+
+root = tb.Window(themename="darkly")
+root.title("Student Registeration System")
+root.geometry("1080x720")
+
+root.mainloop()
