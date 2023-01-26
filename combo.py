7f14d31774c9acd4faa07300ea6b61b3ecb7a2f3
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 19:45:10 +0300

    Created combobox binding function

--- combo.py
+++ combo.py
@@ -10,6 +10,12 @@ root.geometry("500x350")
 def clicker():
     my_label.config(text=f"You clicked {my_combo.get()}!")
 
+# Create Binding Function
+
+
+def click_bind(e):
+    my_label.config(text=f"You clicked {my_combo.get()}!")
+
 
 my_label = tb.Label(root, text="Hello World", font=('Helvetica', 18))
 my_label.pack(pady=30)
@@ -29,5 +35,8 @@ my_combo.current(0)
 my_button = tb.Button(root, text="Click Me!",
                       command=clicker, bootstyle="danger")
 my_button.pack(pady=20)
+
+# Bind the combobox
+my_combo.bind("<<ComboboxSelected>>", click_bind)
 
 root.mainloop()
