351cf8ae95304dcfd2fab7e9ba4bf19b51ab1c3b
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 20:19:32 +0300

    created start button

--- floodguage.py
+++ floodguage.py
@@ -11,5 +11,9 @@ my_gauue = tb.Floodgauge(root, bootstyle="success",
                          font=("Helvetica", 18), mask="Pos: {}%", maximum=100, orient="horizontal", value=10)
 my_gauue.pack(pady=50, fill=X, padx=20)
 
+# Create Start Button
+start_button = tb.Button(root, text="Start", bootstyle="warning outline")
+start_button.pack(pady=20)
+
 
 root.mainloop()
