a303e6deabd3a286fac6df044ec9ebd20843fad8
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 20:46:39 +0300

    created label 2

--- ttkbootsrapp Login/login.py
+++ ttkbootsrapp Login/login.py
@@ -13,4 +13,10 @@ title = tb.Label(root, text="Welcome Cipher!", font=(
     "Heveltica", 25), bootstyle="light")
 title.pack(pady=20)
 
+label2 = tb.Label(
+    root, text="Fill the information below to login", font=("Heveltica", 9))
+label2.pack(pady=10)
+
+# Create entry widgets
+
 root.mainloop()
