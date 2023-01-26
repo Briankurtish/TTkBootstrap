18d775a507add824a3a293d887d51486813c6abc
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-27 02:24:41 +0300

    created title label

--- CRUD Student Management/main.py
+++ CRUD Student Management/main.py
@@ -13,5 +13,14 @@ from tkinter import messagebox
 root = tb.Window(themename="darkly")
 root.title("Student Registeration System")
 root.geometry("1080x720")
+mt_tree = ttk.Treeview(root)
+
+# Functions
+
+# Gui
+label = tb.Label(root, text="Student Registration System",
+                 font=("Arial Bold", 30))
+label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
+
 
 root.mainloop()
