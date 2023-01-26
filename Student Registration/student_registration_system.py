405ac38aeaed4ec78646499b1c29b4287f72c561
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 23:19:58 +0300

    created student id label

--- Student Registration/student_registration_system.py
+++ Student Registration/student_registration_system.py
@@ -8,9 +8,14 @@ root.geometry("500x600")
 
 head_frame = tb.Frame(root)
 
+# Heading label
 heading_lb = tb.Label(head_frame, text="Student Registration System", font=(
     "Bold", 13), bootstyle="danger")
 heading_lb.pack(pady=5)
+
+# Student Id label
+student_id_lb = tb.Label(head_frame, text="Student Id: ", font=("Bold", 10))
+student_id_lb.place(x=0, y=50)
 
 head_frame.pack(pady=10)
 # When you use pack_propagate you can resize the Frame like a container and give it a width and height
