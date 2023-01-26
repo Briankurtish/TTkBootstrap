5e603765091ebb2dac46a914ad9f62eb2a992ff4
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 21:43:50 +0300

    created starter code

---
+++ Student Registration/student_registration_system.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import ttkbootstrap as tb
+
+
+root = tb.Window(themename="superhero")
+root.title("Student registration System")
+root.geometry("500x600")
+
+
+root.mainloop()
