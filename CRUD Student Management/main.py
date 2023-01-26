e74c694fd600fc155fb601631517bbcf349fecdc
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-27 02:31:41 +0300

    created labels

--- CRUD Student Management/main.py
+++ CRUD Student Management/main.py
@@ -22,5 +22,20 @@ label = tb.Label(root, text="Student Registration System",
                  font=("Arial Bold", 30))
 label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
 
+studidLabel = tb.Label(root, text="Stud ID", font=("Arial", 15))
+studidLabel.grid(row=3, column=0, columnspan=1, padx=50, pady=5)
+
+fnameLabel = tb.Label(root, text="First Name", font=("Arial", 15))
+fnameLabel.grid(row=4, column=0, columnspan=1, padx=50, pady=5)
+
+lnameLabel = tb.Label(root, text="Last Name", font=("Arial", 15))
+lnameLabel.grid(row=5, column=0, columnspan=1, padx=50, pady=5)
+
+addressLabel = tb.Label(root, text="Address", font=("Arial", 15))
+addressLabel.grid(row=6, column=0, columnspan=1, padx=50, pady=5)
+
+phoneLabel = tb.Label(root, text="Phone", font=("Arial", 15))
+phoneLabel.grid(row=7, column=0, columnspan=1, padx=50, pady=5)
+
 
 root.mainloop()
