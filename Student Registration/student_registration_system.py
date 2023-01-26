35a634e0724aa9acb0aa016bd195884800fb52ac
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 21:47:25 +0300

    made the head frame to be resizable

--- Student Registration/student_registration_system.py
+++ Student Registration/student_registration_system.py
@@ -6,5 +6,11 @@ root = tb.Window(themename="superhero")
 root.title("Student registration System")
 root.geometry("500x600")
 
+head_frame = tb.Frame(root)
+head_frame.pack(pady=10)
+# When you use pack_propagate you can resize the Frame like a container and give it a width and height
+head_frame.pack_propagate(False)
+head_frame.config(width=500, height=600)
+
 
 root.mainloop()
