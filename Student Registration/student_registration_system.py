64b93d50fa6357a34617181f2930395d56d4f8c4
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-26 23:42:25 +0300

    created register button

--- Student Registration/student_registration_system.py
+++ Student Registration/student_registration_system.py
@@ -19,7 +19,7 @@ student_id_lb.place(x=0, y=50)
 
 # Student Id Entry
 student_id = tb.Entry(head_frame, font=("Bold", 10), bootstyle="info")
-student_id.place(x=120, y=50, width=180)
+student_id.place(x=130, y=50, width=180)
 
 # Student Name
 student_name_lb = tb.Label(
@@ -28,7 +28,7 @@ student_name_lb.place(x=0, y=100)
 
 # Student Name entry
 student_name = tb.Entry(head_frame, font=("Bold", 10), bootstyle="info")
-student_name.place(x=120, y=100, width=180)
+student_name.place(x=130, y=100, width=180)
 
 
 # Student Email
@@ -38,7 +38,22 @@ student_email_lb.place(x=0, y=150)
 
 # Student Email entry
 student_email = tb.Entry(head_frame, font=("Bold", 10), bootstyle="info")
-student_email.place(x=120, y=150, width=180)
+student_email.place(x=130, y=150, width=180)
+
+
+# Student Courses
+student_courses_lb = tb.Label(
+    head_frame, text="Student courses: ", font=("Bold", 10))
+student_courses_lb.place(x=0, y=200)
+
+# Student Courses entry
+student_courses = tb.Entry(head_frame, font=("Bold", 10), bootstyle="info")
+student_courses.place(x=130, y=200, width=180)
+
+# Register Button
+register_btn = tb.Button(head_frame, text='Register',
+                         bootstyle="success outline")
+register_btn.place(x=0, y=250)
 
 
 head_frame.pack(pady=10)
