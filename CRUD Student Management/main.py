b8c9a31c07a78ef01d70c2ac2751e0de90116ece
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-27 17:36:46 +0300

    created the update function

--- CRUD Student Management/main.py
+++ CRUD Student Management/main.py
@@ -179,6 +179,44 @@ def search():
         messagebox.showinfo("Error", "No Data Found")
 
 
+def update():
+    selectedStudid = ""
+
+    try:
+        selected_item = record_table.selection()[0]
+        selectedStudid = str(record_table.item(selected_item)['values'][0])
+    except:
+        messagebox.showinfo("Error", "Please select a data row")
+
+    studid = str(studidEntry.get())
+    fname = str(fnameEntry.get())
+    lname = str(lnameEntry.get())
+    address = str(addressEntry.get())
+    phone = str(phoneEntry.get())
+
+    if (studid == "" or studid == " ") or (fname == "" or fname == " ") or (lname == "" or lname == " ") or (address == "" or address == " ") or (phone == "" or phone == " "):
+        messagebox.showinfo("Error", "Please fill up the blank entry")
+        return
+    else:
+        try:
+            conn = connection()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE students SET STUDID='" +
+                           studid+"', FNAME='" +
+                           fname+"', LNAME='" +
+                           lname+"', ADDRESS='" +
+                           address+"', PHONE='" +
+                           phone+"' WHERE STUDID='" +
+                           selectedStudid+"' ")
+            conn.commit()
+            conn.close()
+        except:
+            messagebox.showinfo("Error", "Stud ID already exist")
+            return
+
+    refreshTable()
+
+
 # Labels
 label = tb.Label(root, text="Student Registration System",
                  font=("Arial Bold", 30), bootstyle='primary')
@@ -230,7 +268,8 @@ add_btn = tb.Button(root, text="Add", width=20,
                     style="success", command=add)
 add_btn.grid(row=3, column=5, columnspan=1, rowspan=2, padx=90, pady=25)
 
-update_btn = tb.Button(root, text="Update", width=20, bootstyle="info")
+update_btn = tb.Button(root, text="Update", width=20,
+                       bootstyle="info", command=update)
 update_btn.grid(row=4, column=5, columnspan=1, rowspan=2)
 
 delete_btn = tb.Button(root, text="Delete", width=20,
