613e4b139cb9ad41aa678c031048104e8b93c7be
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-01-27 16:20:55 +0300

    finish the add function

--- CRUD Student Management/main.py
+++ CRUD Student Management/main.py
@@ -22,14 +22,14 @@ def refreshTable():
                             text="", values=(array), tag="orow")
         record_table.tag_configure(
             'orow', background='#EEEEEE', font=("Arial", 12))
-        record_table.grid(row=8, column=0, columnspan=5,
+        record_table.grid(row=8, column=0, columnspan=4,
                           rowspan=11, padx=10, pady=20)
 
 
 # GUI
-root = tb.Window(themename="darkly")
+root = tb.Window(themename="lumen")
 root.title("Student Registeration System")
-root.geometry("1080x720")
+root.geometry("1200x720")
 
 
 # Functions
@@ -72,7 +72,7 @@ def add():
 
 # Labels
 label = tb.Label(root, text="Student Registration System",
-                 font=("Arial Bold", 30))
+                 font=("Arial Bold", 30), bootstyle='primary')
 label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
 
 studidLabel = tb.Label(root, text="Stud ID", font=("Arial", 15))
@@ -92,52 +92,56 @@ phoneLabel.grid(row=7, column=0, columnspan=1, padx=50, pady=5)
 
 
 # entries
-studidEntry = tb.Entry(root, width=55)
+studidEntry = tb.Entry(root, width=55, bootstyle='info')
 studidEntry.grid(row=3, column=1, columnspan=4, padx=5, pady=0)
 
-fnameEntry = tb.Entry(root, width=55)
+fnameEntry = tb.Entry(root, width=55, bootstyle='info')
 fnameEntry.grid(row=4, column=1, columnspan=4, padx=5, pady=0)
 
-lnameEntry = tb.Entry(root, width=55)
+lnameEntry = tb.Entry(root, width=55, bootstyle='info')
 lnameEntry.grid(row=5, column=1, columnspan=4, padx=5, pady=0)
 
-addressEntry = tb.Entry(root, width=55)
+addressEntry = tb.Entry(root, width=55, bootstyle='info')
 addressEntry.grid(row=6, column=1, columnspan=4, padx=5, pady=0)
 
-phoneEntry = tb.Entry(root, width=55)
+phoneEntry = tb.Entry(root, width=55, bootstyle='info')
 phoneEntry.grid(row=7, column=1, columnspan=4, padx=5, pady=0)
 
 # style
 style = ttk.Style()
-style.configure("Treeview.Heading", font=("Arial Bold", 25))
+style.configure("Treeview.Heading", font=("Arial Bold", 14))
+style.configure("Treeview", background="silver",
+                foreground="black", fieldbackground="green")
+
+style.map("Treeview", background=[('selected', 'green')])
 
 # Buttons
 
 add_btn = tb.Button(root, text="Add", width=20,
-                    style="success outline", command=add)
+                    style="success", command=add)
 add_btn.grid(row=3, column=5, columnspan=1, rowspan=2, padx=90, pady=25)
 
-update_btn = tb.Button(root, text="Update", width=20, bootstyle="info outline")
+update_btn = tb.Button(root, text="Update", width=20, bootstyle="info")
 update_btn.grid(row=4, column=5, columnspan=1, rowspan=2)
 
 delete_btn = tb.Button(root, text="Delete", width=20,
-                       bootstyle="danger outline")
+                       bootstyle="danger")
 delete_btn.grid(row=5, column=5, columnspan=1, rowspan=2, padx=90, pady=25)
 
 search_btn = tb.Button(root, text="Search", width=20,
-                       bootstyle="primary outline")
+                       bootstyle="primary")
 search_btn.grid(row=6, column=5, columnspan=1, rowspan=2, padx=90, pady=25)
 
 reset_btn = tb.Button(root, text="Reset", width=20,
-                      bootstyle="warning outline")
+                      bootstyle="warning")
 reset_btn.grid(row=7, column=5, columnspan=1, rowspan=2, padx=90, pady=25)
 
 select_btn = tb.Button(root, text="Select", width=20,
-                       bootstyle="secondary outline")
+                       bootstyle="secondary")
 select_btn.grid(row=8, column=5, columnspan=1, rowspan=2, padx=90, pady=25)
 
 
-record_table = ttk.Treeview(root)
+record_table = ttk.Treeview(root, bootstyle="primary")
 
 
 record_table["column"] = ['Stud ID',
